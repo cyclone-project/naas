b8bc66f5434e6e3a4e19b0eb2cfb1c5406288eaa
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2016-04-08 14:43:38 +0200

    copyrights

--- src/bash/openvpn-postint.sh
+++ src/bash/openvpn-postint.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+#=============================================================================
+#
+#  File      : openvpn-postint.sh
+#  Date      : March 27th, 2016
+#  Author    : Oleg Lodygensky
+#
+#  Change log:
+#    Jan 13th, 2016; first version
+#
+#
+# Copyright 2016  CNRS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+#=============================================================================
+
 
 ERR_OK=0
 ERR_APTGET=1
